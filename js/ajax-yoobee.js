701d22e371512663c0d29b1a73cc38225dd1673e
Kristy Ramage <kristy.ramage@gmail.com>
2016-01-22 15:23:23 +1300

    use ajax to connect to yoobee.net

--- js/ajax-yoobee.js
+++ js/ajax-yoobee.js
@@ -1,1 +1,27 @@
-alert('Hi');
+$(document).ready(function(){
+	
+	// Listen to the form
+	$('#note-form').submit( function( e ){
+
+		// Prevent form from submitting
+		e.preventDefault();
+
+		// Garb the note from the form
+		if( $.trim( $('#note').val() ) == '' ){
+			return;
+		}
+
+		// Prepare AJAX
+		$.ajax( {
+			url: 'http://kristy.ramage.yoobee.net.nz/ajax-yoobee/ajax-yoobee.php',
+			data: { note: $('#note').val() },
+			success: function( dataFromServer ) {
+
+				},
+			error: function(){
+				alert('Cannot Connect');
+			}
+		});
+
+	});
+});
